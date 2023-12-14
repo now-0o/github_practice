5ff073eca24ff6d78617f2c980dccf7090d0dfb4
now-0o <dlwpdn0704@gmail.com>
2023-12-14 20:53:22 +0900

    feat: 로그인 라우트 추가

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/sign-in', (req, res) => {
+  res.send('login success!');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
